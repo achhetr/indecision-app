5c0d3b77b5c716a2b00c3afcef2dc4a6c41c65d7
Akash Chhetri <akash.chhetri1@gmail.com>
2020-09-22 07:52:34 +1000

    babel setup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,12 @@
 console.log('App.js is running');
 
-// JS - JavaScript XML
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+	'p',
+	{ id: 'someId' },
+	'This is JSX from app.js'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
